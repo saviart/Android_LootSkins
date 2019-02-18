9c3fae0484920b7710c2e9161369f05a18133206
saviart <quangson1909@gmail.com>
2019-02-18 20:38:10 +0700

    Update icon - Animate
    Update icon - Animate

--- app/src/main/java/com/saviart/lootskins/MainActivity.java
+++ app/src/main/java/com/saviart/lootskins/MainActivity.java
@@ -1,11 +1,19 @@
-package com.saviart.lootskills;
+package com.saviart.lootskins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnSignup;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
+        btnSignup = (ImageButton) findViewById(R.id.btnSignup);
+        Animation animsighup = AnimationUtils.loadAnimation(this,R.anim.animbtnsignup);
+        btnSignup.startAnimation(animsighup);
 
 
         /// test github
