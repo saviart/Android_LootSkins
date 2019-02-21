4536be1e7e4867b284d75d550231dcdea5fa1661
saviart <quangson1909@gmail.com>
2019-02-21 15:55:17 +0700

    Navigate Done

--- app/src/main/java/com/saviart/lootskins/SkinActivity.java
+++ app/src/main/java/com/saviart/lootskins/SkinActivity.java
@@ -1,14 +1,59 @@
 package com.saviart.lootskins;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gigamole.navigationtabstrip.NavigationTabStrip;
 
 
 public class SkinActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private NavigationTabStrip mNavigationTabStrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skin);
+        initUI();
+        setUI();
+    }
+
+    private void initUI() {
+        mViewPager = (ViewPager) findViewById(R.id.vp);
+
+        mNavigationTabStrip = (NavigationTabStrip) findViewById(R.id.nts_center);
+        mNavigationTabStrip.setTitles("A", "B", "C");
+
+    }
+
+    private void setUI() {
+        mViewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 3;
+            }
+
+            @Override
+            public boolean isViewFromObject(final View view, final Object object) {
+                return view.equals(object);
+            }
+
+            @Override
+            public void destroyItem(final View container, final int position, final Object object) {
+                ((ViewPager) container).removeView((View) object);
+            }
+
+            @Override
+            public Object instantiateItem(final ViewGroup container, final int position) {
+                final View view = new View(getBaseContext());
+                container.addView(view);
+                return view;
+            }
+        });
+        mNavigationTabStrip.setViewPager(mViewPager, 1);
     }
 }
